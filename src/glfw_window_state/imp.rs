e0ebc2a255496b5d488236301a7dd089fb46d975
RustUser <alcandroiddev@gmail.com>
2024-06-15 02:25:23 -0400

    Added getters for window state.

--- src/glfw_window_state/imp.rs
+++ src/glfw_window_state/imp.rs
@@ -1,7 +1,29 @@
+use glfw::{Glfw, Window};
+use crate::glfw_clock::GlfwClock;
+use crate::glfw_input::GlfwInput;
 use crate::glfw_window_state::GlfwWindowState;
 
 impl <'a> GlfwWindowState<'a> {
     pub fn force_next_render(&mut self, render: bool) {
         self.force_render = render;
+    }
+
+    pub fn glfw(&self) -> &'a mut Glfw {
+        self.glfw
+    }
+    pub fn window(&self) -> &'a mut Window {
+        self.window
+    }
+    pub fn input(&self) -> &'a mut GlfwInput {
+        self.input
+    }
+    pub fn clock(&self) -> &'a GlfwClock {
+        self.clock
+    }
+    pub fn window_position(&self) -> [i32; 2] {
+        self.window_position
+    }
+    pub fn force_render(&self) -> bool {
+        self.force_render
     }
 }
